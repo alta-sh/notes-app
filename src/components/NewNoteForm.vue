8f176b41c5c9c1b74be2055944f50b36b34e2b87
AlexAshley <aa96sk@gmial.com>
2022-01-23 02:14:31 +0000

    finalise new note modal

--- src/components/NewNoteForm.vue
+++ src/components/NewNoteForm.vue
@@ -9,27 +9,60 @@
     >
       <v-card>
         <v-row justify="space-between" class="ma-1">
-          <v-card-title class="text-h5">
-            Create a new note:
-          </v-card-title>
-          <v-btn @click="closeModal()" small class="mt-2 mx-2" icon color="red">
-            <v-icon large>mdi-close</v-icon>
-          </v-btn>
+          <v-spacer/>
+          
         </v-row>
 
 
-        <v-card-text>
-          form...
+        <v-card-text class="ma-0 pl-2 pr-1 pr-sm-1 pt-1">
+          <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          >
+            <v-row class="ma-0">
+              <v-col cols="6">
+                <v-text-field
+                    v-model="noteTitle"
+                    required
+                    placeholder="Note Title"
+                    clearable
+                    autofocus
+                    solo
+                    class="mt-3"
+                    dense
+                />
+              </v-col>
+              
+              <v-col cols="5">
+                <p class="text-sm-center font-weight-bold blue-grey--text text--lighten-4">{{dateTime}}</p>
+              </v-col>
+              
+              
+              <v-col cols="1" class="pt-1 pr-1">
+                <v-btn @click="closeModal()" class="float-right" small icon color="red">
+                <v-icon large>mdi-close</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+            
+            
+            <v-textarea
+                v-model="noteContent"
+                placeholder="Note Content"
+                dense
+                class="px-8"
+            />
+          </v-form>
         </v-card-text>
 
         <v-card-actions class="justify-space-between">
-
           <v-btn
               color="red darken-1"
               text
               @click="resetForm()"
           >
-            reset form
+            reset
           </v-btn>
 
           <v-btn
@@ -51,6 +84,11 @@ export default {
   
   data() {
     return {
+      valid: true,
+      noteTitle: "",
+      noteContent: "",
+      dateTime: {},
+      timer: {},
     }
   },
   
@@ -63,25 +101,38 @@ export default {
       get(){
         return this.modal;
       },
-    }
+    },
   },
   
+  created() {
+    this.dateTime = new Date();
+    this.timer = setInterval(() => {
+      var date = new Date();
+      this.dateTime = date.toUTCString().slice(0,-3) ;
+    }, 1000);
+  },
+
   methods: {
+    
     closeModal() {
-      this.$emit("closeModal")
+      this.$emit("closeModal");
+      this.resetForm();
     },
       
     resetForm() {
-      console.log("resetting form")
+      this.noteTitle = "";
+      this.noteContent = "";
     },
     
     submitForm() {
       console.log("submitting form")
-    }  
+    } 
   }
 }
 </script>
 
 <style scoped>
-
+.border {
+  border-bottom: 1px solid rgba(255, 138, 101, 0.4);
+}
 </style>
